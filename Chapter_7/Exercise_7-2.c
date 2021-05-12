eacbbf91530f6ee283c6c38ba39e549080df8646
briansb <4brianbirmingham@gmail.com>
2021-05-12 09:37:48 -0500

    updates

--- Chapter_7/Exercise_7-2.c
+++ Chapter_7/Exercise_7-2.c
@@ -2,18 +2,25 @@
 #include <string.h>
 
 #define MAXLINE 100
+#define MAXCOLUMNS 20
 
 int main()   /* lower:  convert input to lower case */
 {
     int c;
     char line[MAXLINE];
     char single_character[1];
+    int column_counter = 0;
 
     while ((c = getchar()) != EOF) {
         if ( c <= 126 && c >= 32) { /* printable character */
             single_character[0] = c;
             strncat(line, single_character, 1);
-            
+            column_counter++;
+            if (column_counter >= MAXCOLUMNS) {
+                printf("%s...\n", line);
+                line[0]='\0';
+                column_counter = 0;
+            }
         }
 
 
