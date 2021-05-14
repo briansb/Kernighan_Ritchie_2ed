acfa7da8a99762941ef131af703095aec35f8cc3
briansb <4brianbirmingham@gmail.com>
2021-05-14 09:27:13 -0500

    updates

--- Chapter_7/Exercise_7-2.c
+++ Chapter_7/Exercise_7-2.c
@@ -4,11 +4,14 @@
 #define MAXLINE 100
 #define MAXCOLUMNS 20
 
+int get_hex_from_int(char *, int);
+
 int main()   /* lower:  convert input to lower case */
 {
     int c;
     char line[MAXLINE];
     char single_character[1];
+    char hex_char[4];
     int column_counter = 0;
 
     while ((c = getchar()) != EOF) {
@@ -21,13 +24,35 @@ int main()   /* lower:  convert input to lower case */
                 line[0]='\0';
                 column_counter = 0;
             }
+        } else {
+            get_hex_from_int(hex_char,c);
+            strncat(line, hex_char, 4);
+            column_counter += 4;
+            if (column_counter >= MAXCOLUMNS) {
+                printf("%s...\n", line);
+                line[0]='\0';
+                column_counter = 0;
+            }
         }
-
-
     }
     
     /* print last line */
     printf("%s\n", line);
     return 0;
+}
+int get_hex_from_int(char *hex_char, int c) {
+	char *w = hex_char;
 
+	*w = '#';
+    w++;
+    /* left digit */
+    *w = (c / 16) + 48;
+    w++;
+    /* right digit */
+    if ((c % 16) < 10 ) *w = (c % 16) + 48;
+    else *w = (c % 16) + 87;
+    w++;
+    *w = '#';
+
+	return hex_char[0];
 }
